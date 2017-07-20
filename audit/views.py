3239c2c6ae9d7cdadfada8a52a16fc28d7ed632a
jack <jack@lab-netscanner01.lab2.mckinsey.com>
2017-07-20 14:07:10 -0400

    Try

--- audit/views.py
+++ audit/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+def post_list(request):
+    return render(request, 'audit/index.html',{})
 
 def people(request):
     return render(request, 'audit/index.html', {'people': Person.objects.all()})
